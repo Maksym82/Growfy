a5cd287bdd7c3b18f3e9d28eeedef6c6ecb788a7
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-01-31 08:27:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,24 @@ function documentClick(e) {
     }
 }
 
+
+// Scroll up
+
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+scrollToTopButton.addEventListener("click", () => {
+  anime({
+    targets: document.documentElement,
+    scrollTop: 0,
+    duration: 200,
+    easing: "easeInOutQuad",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+});
